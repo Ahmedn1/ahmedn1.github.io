af4caff7b58d1c9ed695bf206674bfb4e49b7562
Ahmed Dawod <ahmed@marketmuse.com>
2026-05-06 01:53:55 -0400

    Add custom domain ahmeddawod.io

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
-// If deploying to https://ahmedn1.github.io/personal-website/, set:
-//   site: 'https://ahmedn1.github.io'
-//   base: '/personal-website'
-// If using a custom domain or the ahmedn1.github.io root repo, set:
-//   site: 'https://ahmedn1.github.io'  (or your custom domain)
-//   and remove `base`
 export default defineConfig({
-  site: 'https://ahmedn1.github.io',
+  site: 'https://ahmeddawod.io',
   integrations: [tailwind(), sitemap()],
 });
